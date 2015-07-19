031527f3fdc470feddbc84a13fe62bf7d25ab7e8
Hiroshi Fukada <hiroshi@fukada.us>
2015-07-18 21:14:16 -0400

    initial commit

---
+++ project.clj
@@ -0,0 +1,8 @@
+(defproject moo "0.1.0-SNAPSHOT"
+  :description "Super Simple Image Hoster using s3 as a storage solution"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                 [ring "1.4.0"]
+                 [clj-aws-s3 "0.3.10"]])
